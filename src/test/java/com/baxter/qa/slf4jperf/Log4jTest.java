2677c4fa5578f2a4f0743da23eb870804088aa76
Mihaly Nyeste <mihaly.nyeste@gmail.com>
2011-05-29 21:48:49 +0200

    Finishing log4j tests

--- src/test/java/com/baxter/qa/slf4jperf/Log4jTest.java
+++ src/test/java/com/baxter/qa/slf4jperf/Log4jTest.java
@@ -2,6 +2,8 @@ package com.baxter.qa.slf4jperf;
 
 import static org.junit.Assert.*;
 
+import java.util.UUID;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -11,7 +13,8 @@ import org.junit.Test;
 public class Log4jTest {
 
 	private static Logger logger = Logger.getLogger(Log4jTest.class);
-	private static final int LOG_ENTRIES = 1000;
+	private static final int LOG_ENTRIES = 10000;
+	private static String logString = UUID.randomUUID().toString();
 
 	@BeforeClass
 	public static void setUp() {
@@ -21,16 +24,33 @@ public class Log4jTest {
 	@Test
 	public void testBaseLogging() {
 		logger.setLevel(Level.INFO);
-		logger.info("Info log");
-		logger.debug("Debug log");
-		assertTrue(true);
+		logger.info("Info message");
+		logger.debug("Debug message");
 	}
 
 	@Test
 	public void testLogStringConcat() {
 		logger.setLevel(Level.INFO);
-		for (int i=1; i<=LOG_ENTRIES; ++i){
-			logger.info("Info message no." + i);
+		for (int i = 1; i <= LOG_ENTRIES; ++i) {
+			logger.info("Info message no." + i + " UUID: " + logString);
 		}
 	}
+
+	@Test
+	public void testNoLogStringConcat() {
+		logger.setLevel(Level.INFO);
+		for (int i = 1; i <= LOG_ENTRIES; ++i) {
+			logger.debug("Debug message no." + i + " UUID: " + logString);
+		}
+	}
+
+	@Test
+	public void testNoLogExtraIfStringConcat() {
+		logger.setLevel(Level.INFO);
+		for (int i = 1; i <= LOG_ENTRIES; ++i) {
+			if (logger.getLevel() == Level.DEBUG)
+				logger.debug("Debug message no." + i + " UUID: " + logString);
+		}
+	}
+
 }
